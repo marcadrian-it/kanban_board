ecd2d62c6767084d1b5d8623a10f78edc7c2f2bd
marcadrian-it <marcadrian.it@gmail.com>
2023-05-15 09:49:00 +0200

    working kanban board

---
+++ js/view/Column.js
@@ -0,0 +1,53 @@
+import KanbanAPI from "../api/KanbanAPI.js";
+import Item from "./Item.js";
+import DropZone from "./DropZone.js";
+
+export default class Column {
+  constructor(id, title) {
+    const topDropZone = DropZone.createDropZone();
+
+    this.elements = {};
+    this.elements.root = Column.createRoot();
+    this.elements.title = this.elements.root.querySelector(
+      ".kanban-column-title"
+    );
+    this.elements.items = this.elements.root.querySelector(
+      ".kanban-column-items"
+    );
+    this.elements.addItem =
+      this.elements.root.querySelector(".kanban-add-item");
+
+    this.elements.root.dataset.id = id;
+    this.elements.title.textContent = title;
+    this.elements.items.appendChild(topDropZone);
+
+    this.elements.addItem.addEventListener("click", () => {
+      const newItem = KanbanAPI.insertItem(id, "");
+      this.renderItem(newItem);
+    });
+
+    KanbanAPI.getItems(id).forEach((item) => {
+      this.renderItem(item);
+    });
+  }
+
+  static createRoot() {
+    const range = document.createRange();
+
+    range.selectNode(document.body);
+
+    return range.createContextualFragment(`
+    <div class="kanban-column">
+    <div class="kanban-column-title"></div>
+    <div class="kanban-column-items"></div>
+        <button class="kanban-add-item" type="button">+ Add</button>
+        </div>
+    `).children[0];
+  }
+
+  renderItem(data) {
+    const item = new Item(data.id, data.content);
+
+    this.elements.items.appendChild(item.elements.root);
+  }
+}
